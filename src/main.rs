4ff7d480873d064d3700e9634c1319d4be30f2d1
Ayush Muley <ayush.muley1907@outlook.com>
2026-04-24 07:17:40 +0000

    not much

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,9 @@ mod lexer;
 mod parser;
 mod repl;
 
+use interpreter::file_runner::run_file;
 use repl::repl;
 use std::env;
-use interpreter::file_runner::run_file;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -13,6 +13,8 @@ fn main() {
     if args.len() > 1 {
         let file_path = &args[1];
         run_file(file_path);
+    } else if args.len() > 2 {
+        eprintln!("Usage: {} [file]", args[0]);
     } else {
         repl();
     }
